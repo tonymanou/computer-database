a5452c46df6a85c94feb32e0cd0f69d606287ef7
tonymanou <tonymanou@gmail.com>
2015-01-15 17:55:38 +0100

    whoops

--- src/test/java/com/tonymanou/computerdb/dao/impl/CompanyDAOTest.java
+++ src/test/java/com/tonymanou/computerdb/dao/impl/CompanyDAOTest.java
@@ -1,27 +1,14 @@
 package com.tonymanou.computerdb.dao.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.verify;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import com.tonymanou.computerdb.entity.Company;
 
-@RunWith(MockitoJUnitRunner.class)
-public class CompanyDAOTest {
+public class CompanyDAOTest extends SQLDAOTest {
 
   private static SQLCompanyDAO companyDAO;
   private static Company company1;
   private static Company company2;
-
-  @BeforeClass
-  public static void setUp() {
-    companyDAO = new SQLCompanyDAO();
-  }
 
   @Test
   public void findAllTest() {
