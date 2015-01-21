ac2f771f77b0c2e68bad8e2967f955f4b840c5df
tonymanou <tonymanou@gmail.com>
2015-01-21 11:01:28 +0100

    Added list mappers

--- src/main/java/com/tonymanou/computerdb/mapper/CompanyMapper.java
+++ src/main/java/com/tonymanou/computerdb/mapper/CompanyMapper.java
@@ -1,9 +1,34 @@
 package com.tonymanou.computerdb.mapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.tonymanou.computerdb.dto.CompanyDTO;
 import com.tonymanou.computerdb.entity.Company;
 
 public class CompanyMapper {
+
+  public static List<CompanyDTO> mapToDTOList(List<Company> list) {
+    if (list == null) {
+      return null;
+    }
+    // @formatter:off
+    return list.stream()
+        .map(e -> mapToDTO(e))
+        .collect(Collectors.toList());
+    // @formatter:on
+  }
+
+  public static List<Company> mapToList(List<CompanyDTO> list) {
+    if (list == null) {
+      return null;
+    }
+    // @formatter:off
+    return list.stream()
+        .map(e -> mapToCompany(e))
+        .collect(Collectors.toList());
+    // @formatter:on
+  }
 
   public static CompanyDTO mapToDTO(Company company) {
     // @formatter:off
--- src/main/java/com/tonymanou/computerdb/mapper/ComputerMapper.java
+++ src/main/java/com/tonymanou/computerdb/mapper/ComputerMapper.java
@@ -1,6 +1,8 @@
 package com.tonymanou.computerdb.mapper;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.tonymanou.computerdb.dto.ComputerDTO;
 import com.tonymanou.computerdb.entity.Company;
@@ -8,6 +10,28 @@ import com.tonymanou.computerdb.entity.Computer;
 import com.tonymanou.computerdb.util.Util;
 
 public class ComputerMapper {
+
+  public static List<ComputerDTO> mapToDTOList(List<Computer> list) {
+    if (list == null) {
+      return null;
+    }
+    // @formatter:off
+    return list.stream()
+        .map(e -> mapToDTO(e))
+        .collect(Collectors.toList());
+    // @formatter:on
+  }
+
+  public static List<Computer> mapToList(List<ComputerDTO> list) {
+    if (list == null) {
+      return null;
+    }
+    // @formatter:off
+    return list.stream()
+        .map(e -> mapToComputer(e))
+        .collect(Collectors.toList());
+    // @formatter:on
+  }
 
   public static ComputerDTO mapToDTO(Computer computer) {
     LocalDate introduced = computer.getIntroduced();
