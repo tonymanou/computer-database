af65bd0d60aa67501525731fe8603793fd93cf42
tonymanou <tonymanou@gmail.com>
2015-01-22 17:40:52 +0100

    Ability to delete some computers

--- src/main/java/com/tonymanou/computerdb/controller/DashboardController.java
+++ src/main/java/com/tonymanou/computerdb/controller/DashboardController.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.tonymanou.computerdb.domain.Computer;
 import com.tonymanou.computerdb.dto.ComputerDTO;
 import com.tonymanou.computerdb.mapper.IEntityMapper;
@@ -23,6 +26,7 @@ import com.tonymanou.computerdb.util.Util;
 public class DashboardController extends HttpServlet {
 
   private static final long serialVersionUID = 1075773814185556399L;
+  private static final Logger LOGGER = LoggerFactory.getLogger(DashboardController.class);
   private static final String ELEMENT_PER_PAGE = "element_per_page";
 
   private IComputerService computerService;
@@ -55,5 +59,29 @@ public class DashboardController extends HttpServlet {
     req.setAttribute("computers", computers);
     req.setAttribute("page", page);
     req.getRequestDispatcher("/WEB-INF/views/dashboard.jsp").forward(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+      IOException {
+    String idList = req.getParameter("selection");
+
+    if (idList != null) {
+      String[] idsList = idList.split(",");
+
+      for (String current : idsList) {
+        Long id = Util.parseLong(current);
+
+        if (id != null) {
+          computerService.delete(id);
+        } else {
+          LOGGER.warn("Unparsable id: " + current);
+        }
+      }
+    } else {
+      LOGGER.error("Empty selection list");
+    }
+
+    doGet(req, resp);
   }
 }
