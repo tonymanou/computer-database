d82a0b75d0ca3c78ef61be8062833dc3f648667f
tonymanou <tonymanou@gmail.com>
2015-02-17 15:27:02 +0100

    [Webclient] Split CLI in two

--- computerdb-webclient/src/main/java/com/tonymanou/computerdb/webclient/CLIRoutine.java
+++ computerdb-webclient/src/main/java/com/tonymanou/computerdb/webclient/CLIRoutine.java
@@ -1,18 +1,13 @@
 package com.tonymanou.computerdb.webclient;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.tonymanou.computerdb.dto.CompanyDTO;
 import com.tonymanou.computerdb.dto.ComputerDTO;
-import com.tonymanou.computerdb.util.Util;
+import com.tonymanou.computerdb.webclient.ScannerHelper.EmptyType;
 import com.tonymanou.computerdb.webservice.ICompanyWS;
 import com.tonymanou.computerdb.webservice.IComputerWS;
 
@@ -25,38 +20,11 @@ import com.tonymanou.computerdb.webservice.IComputerWS;
 public class CLIRoutine {
 
   private static final String UNRECOGNIZED = "Unrecognized action.";
-  private static final String CANCELED = "Input canceled...";
-  private static final String MSG_DATE_FORMAT = "date.format";
 
-  private Scanner scanner;
   private IComputerWS computerService;
   private ICompanyWS companyService;
-
   @Autowired
-  @Qualifier("formatMessageSource")
-  private MessageSource messageSource;
-
-  /**
-   * Enumeration describing the default action to do when the user enters an empty value.
-   */
-  private static enum EmptyType {
-    /**
-     * Leave the field empty.
-     */
-    EMPTY,
-    /**
-     * Cancel the current input.
-     */
-    CANCEL,
-    /**
-     * Keep the original value.
-     */
-    KEEP
-  }
-
-  public CLIRoutine() {
-    scanner = new Scanner(System.in);
-  }
+  private ScannerHelper sc;
 
   public void init(IComputerWS computerWS, ICompanyWS companyWS) {
     computerService = computerWS;
@@ -79,12 +47,12 @@ public class CLIRoutine {
       System.out.print("> ");
 
       // Get user's input
-      action = scanner.nextLine();
+      action = sc.nextLine();
 
       System.out.println();
 
       // Split action string into words
-      String[] words = splitToWords(action);
+      String[] words = ScannerHelper.splitToWords(action);
 
       switch (words[0]) {
       case "computer":
@@ -144,8 +112,7 @@ public class CLIRoutine {
       }
     }
 
-    scanner.close();
-    scanner = null;
+    sc.close();
 
     System.out.println("========== End ==========");
   }
@@ -171,23 +138,25 @@ public class CLIRoutine {
    * Let the CLI user add a new computer.
    */
   private void doAddComputer() {
-    String name = getStringInput("[Add computer] Enter the name of the computer.", EmptyType.CANCEL);
+    String name = sc.getStringInput("[Add computer] Enter the name of the computer.",
+        EmptyType.CANCEL);
     if (name == null) {
       return;
     }
     ComputerDTO.Builder builder = ComputerDTO.getBuilder(name);
 
     builder
-        .setIntroduced(getDateInput("[Add computer] Enter its introduced date.", EmptyType.EMPTY));
-    builder.setDiscontinued(getDateInput("[Add computer] Enter its discontinued date.",
+        .setIntroduced(sc
+            .getDateInput("[Add computer] Enter its introduced date.", EmptyType.EMPTY));
+    builder.setDiscontinued(sc.getDateInput("[Add computer] Enter its discontinued date.",
         EmptyType.EMPTY));
 
-    if (isYesAnswer("[Add computer] Do you want to list all the companies first?")) {
+    if (sc.isYesAnswer("[Add computer] Do you want to list all the companies first?")) {
       doListCompanies();
       System.out.println();
     }
 
-    Long companyId = getLongInput("[Add computer] Enter its company ID.", EmptyType.EMPTY);
+    Long companyId = sc.getLongInput("[Add computer] Enter its company ID.", EmptyType.EMPTY);
     if (companyId != null) {
       CompanyDTO company = companyService.getFromId(companyId);
       builder.setCompany(company.getName());
@@ -201,12 +170,12 @@ public class CLIRoutine {
    * Let the CLI user remove a computer.
    */
   private void doRemoveComputer() {
-    if (isYesAnswer("[Remove computer] Do you want to list all the computers first?")) {
+    if (sc.isYesAnswer("[Remove computer] Do you want to list all the computers first?")) {
       doListComputers();
       System.out.println();
     }
 
-    Long id = getLongInput("[Remove computer] Enter the id of the computer you want to delete.",
+    Long id = sc.getLongInput("[Remove computer] Enter the id of the computer you want to delete.",
         EmptyType.CANCEL);
 
     if (id != null) {
@@ -219,12 +188,12 @@ public class CLIRoutine {
    * Let the CLI user update a computer.
    */
   private void doUpdateComputer() {
-    if (isYesAnswer("[Update computer] Do you want to list all the computers first?")) {
+    if (sc.isYesAnswer("[Update computer] Do you want to list all the computers first?")) {
       doListComputers();
       System.out.println();
     }
 
-    Long id = getLongInput("[Update computer] Enter the id of the computer you want to update.",
+    Long id = sc.getLongInput("[Update computer] Enter the id of the computer you want to update.",
         EmptyType.CANCEL);
 
     if (id != null) {
@@ -235,30 +204,31 @@ public class CLIRoutine {
       } else {
         String oldName = computer.getName();
         System.out.println("[Update computer] Current name is '" + oldName + "'.");
-        String name = getStringInput("[Update computer] Enter the new name.", EmptyType.KEEP,
+        String name = sc.getStringInput("[Update computer] Enter the new name.", EmptyType.KEEP,
             oldName);
         computer.setName(name);
 
         String oldIntroduced = computer.getIntroducedDate();
         System.out.println("[Update computer] Current introduced date is " + oldIntroduced + ".");
-        String introduced = getDateInput("[Update computer] Enter the new date.",
+        String introduced = sc.getDateInput("[Update computer] Enter the new date.",
             EmptyType.KEEP, oldIntroduced);
         computer.setIntroducedDate(introduced);
 
         String oldDiscontinued = computer.getDiscontinuedDate();
         System.out.println("[Update computer] Current introduced date is " + oldDiscontinued + ".");
-        String discontinued = getDateInput("[Update computer] Enter the new date.",
+        String discontinued = sc.getDateInput("[Update computer] Enter the new date.",
             EmptyType.KEEP, oldDiscontinued);
         computer.setDiscontinuedDate(discontinued);
 
-        System.out.println("[Update computer] Current company is " + computer.getCompanyName() + ".");
-        if (isYesAnswer("[Update computer] Do you want to list all the companies first?")) {
+        System.out.println("[Update computer] Current company is " + computer.getCompanyName()
+            + ".");
+        if (sc.isYesAnswer("[Update computer] Do you want to list all the companies first?")) {
           doListCompanies();
           System.out.println();
         }
 
         Long oldCompanyId = computer.getCompanyId();
-        Long companyId = getLongInput("[Update computer] Enter the new company company ID.",
+        Long companyId = sc.getLongInput("[Update computer] Enter the new company company ID.",
             EmptyType.KEEP, oldCompanyId);
         CompanyDTO company = null;
         if (companyId != null) {
@@ -294,274 +264,17 @@ public class CLIRoutine {
   }
 
   private void doRemoveCompany() {
-    if (isYesAnswer("[Remove company] Do you want to list all the company first?")) {
+    if (sc.isYesAnswer("[Remove company] Do you want to list all the company first?")) {
       doListCompanies();
       System.out.println();
     }
 
-    Long id = getLongInput("[Remove company] Enter the id of the company you want to delete.",
+    Long id = sc.getLongInput("[Remove company] Enter the id of the company you want to delete.",
         EmptyType.CANCEL);
 
     if (id != null) {
       companyService.delete(id);
       System.out.println("Deleting company [id=" + id + "] and its computers.");
     }
-  }
-
-  /* ========== Scanner helpers ========== */
-
-  /**
-   * Ask the user to enter a string, or nothing to cancel.
-   *
-   * @param message
-   *          The message to display at the beginning of the input.
-   * @param emptyType
-   *          What to do when the user leaves an empty value.
-   * @param original
-   *          Optional original value, only used when using emptyType KEEP.
-   * @return a string, or null if the user canceled the input.
-   */
-  private String getStringInput(String message, EmptyType emptyType, String... original) {
-    String string = null;
-
-    System.out.print(message + " (Enter nothing to ");
-    switch (emptyType) {
-    case EMPTY:
-      System.out.print("leave empty");
-      break;
-    case CANCEL:
-      System.out.print("cancel");
-      break;
-    case KEEP:
-      System.out.print("keep current value, enter 0 to set no value");
-      break;
-    }
-    System.out.print(")\n> ");
-
-    String input = scanner.nextLine();
-    String[] words = splitToWords(input);
-
-    if ("".equals(words[0])) {
-      switch (emptyType) {
-      case EMPTY:
-        System.out.println("Value set to null.");
-        break;
-      case CANCEL:
-        System.out.println(CANCELED);
-        break;
-      case KEEP:
-        System.out.println("Original value kept.");
-        if (original != null && original.length >= 1) {
-          string = original[0];
-        }
-        break;
-      }
-    } else if (emptyType == EmptyType.KEEP && "0".equals(words[0])) {
-      System.out.println("Value set to null.");
-    } else {
-      string = input;
-      System.out.println();
-    }
-
-    return string;
-  }
-
-  /**
-   * Ask the user if he wants to do something.
-   *
-   * @param message
-   *          The message to display to the user.
-   * @return true if the user answered by yes, false otherwise.
-   */
-  private boolean isYesAnswer(String message) {
-    System.out.print(message + " [y|n]\n> ");
-
-    String input = scanner.nextLine();
-    System.out.println();
-
-    if (input == null) {
-      input = "";
-    } else {
-      input.trim().toLowerCase();
-    }
-
-    return input.startsWith("y");
-  }
-
-  /**
-   * Ask the user to enter a date, or nothing to leave empty.
-   *
-   * @param message
-   *          The message to display at the beginning of the input.
-   * @param emptyType
-   *          What to do when the user leaves an empty value.
-   * @param original
-   *          Optional original value, only used when using emptyType KEEP.
-   * @return the date, or null if the the user left the input empty.
-   */
-  private String getDateInput(String message, EmptyType emptyType, String... original) {
-    boolean running = true;
-    String date = null;
-    String string;
-
-    String datePattern = messageSource.getMessage(MSG_DATE_FORMAT, null,
-        LocaleContextHolder.getLocale());
-
-    System.out.print(message + " (" + datePattern + ", enter nothing to ");
-    switch (emptyType) {
-    case EMPTY:
-      System.out.print("leave empty");
-      break;
-    case CANCEL:
-      System.out.print("cancel");
-      break;
-    case KEEP:
-      System.out.print("keep current value, enter 0 to set no value");
-      break;
-    }
-    System.out.println(")");
-
-    // Loop until a valid date is entered or the input is canceled
-    do {
-      System.out.print("> ");
-
-      string = scanner.nextLine();
-      String[] words = splitToWords(string);
-
-      if ("".equals(words[0])) {
-        switch (emptyType) {
-        case EMPTY:
-          System.out.println("Value set to null.");
-          break;
-        case CANCEL:
-          System.out.println(CANCELED);
-          break;
-        case KEEP:
-          System.out.println("Original value kept.");
-          if (original != null && original.length >= 1) {
-            date = original[0];
-          }
-          break;
-        }
-        System.out.println();
-        running = false;
-      } else if (emptyType == EmptyType.KEEP && "0".equals(words[0])) {
-        System.out.println("Value set to null.");
-        System.out.println();
-        running = false;
-      } else {
-        boolean bad = true;
-
-        LocalDate dateIn = Util.parseLocalDate(string, datePattern);
-        if (dateIn == null) {
-          date = null;
-          bad = false;
-          System.out.println();
-        } else {
-          date = string;
-        }
-        if (bad) {
-          System.out.println("Please enter a valid date.");
-        }
-      }
-    } while (date == null && running);
-
-    return date;
-  }
-
-  /**
-   * Ask the user to input a (long) number.
-   *
-   * @param message
-   *          The message to display at the beginning of the input.
-   * @param emptyType
-   *          What to do when the user leaves an empty value.
-   * @param original
-   *          Optional original value, only used when using emptyType KEEP.
-   * @return the input number, or null if the input was canceled.
-   */
-  private Long getLongInput(String message, EmptyType emptyType, Long... original) {
-    boolean running = true;
-    Long number = null;
-    String string;
-
-    System.out.print(message + " (Positive number, enter nothing to ");
-    switch (emptyType) {
-    case EMPTY:
-      System.out.print("leave empty");
-      break;
-    case CANCEL:
-      System.out.print("cancel");
-      break;
-    case KEEP:
-      System.out.print("keep current value, enter 0 to set no value");
-      break;
-    }
-    System.out.println(")");
-
-    // Loop until a valid number is entered or the input is canceled
-    do {
-      System.out.print("> ");
-
-      string = scanner.nextLine();
-      String[] words = splitToWords(string);
-
-      if ("".equals(words[0])) {
-        switch (emptyType) {
-        case EMPTY:
-          System.out.println("Value set to null.");
-          break;
-        case CANCEL:
-          System.out.println(CANCELED);
-          break;
-        case KEEP:
-          System.out.println("Original value kept.");
-          if (original != null && original.length >= 1) {
-            number = original[0];
-          }
-          break;
-        }
-        System.out.println();
-        running = false;
-      } else if (emptyType == EmptyType.KEEP && "0".equals(words[0])) {
-        System.out.println("Value set to null.");
-        System.out.println();
-        running = false;
-      } else {
-        boolean bad = true;
-
-        // Try to parse the number
-        if (words[0].matches("[+]?[0-9]+")) {
-          number = Long.parseLong(words[0]);
-          bad = false;
-          System.out.println();
-        }
-        if (bad) {
-          System.out.println("Please enter a positive number.");
-        }
-      }
-    } while (number == null && running);
-
-    return number;
-  }
-
-  /**
-   * Split a string into words.
-   *
-   * @param string
-   *          The string to split.
-   * @return a non-empty array with all the substrings.
-   */
-  private String[] splitToWords(String string) {
-    String[] words;
-
-    if (string != null) {
-      words = string.trim().toLowerCase().split(" ");
-    } else {
-      words = new String[1];
-      words[0] = "";
-    }
-
-    return words;
   }
 }
---
+++ computerdb-webclient/src/main/java/com/tonymanou/computerdb/webclient/ScannerHelper.java
@@ -0,0 +1,319 @@
+package com.tonymanou.computerdb.webclient;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.tonymanou.computerdb.util.Util;
+
+@Component
+public class ScannerHelper {
+
+  private static final String CANCELED = "Input canceled...";
+  private static final String MSG_DATE_FORMAT = "date.format";
+
+  @Autowired
+  @Qualifier("formatMessageSource")
+  private MessageSource messageSource;
+
+  private Scanner scanner;
+
+  /**
+   * Enumeration describing the default action to do when the user enters an empty value.
+   */
+  public enum EmptyType {
+    /**
+     * Leave the field empty.
+     */
+    EMPTY,
+    /**
+     * Cancel the current input.
+     */
+    CANCEL,
+    /**
+     * Keep the original value.
+     */
+    KEEP
+  }
+
+  public ScannerHelper() {
+    scanner = new Scanner(System.in);
+  }
+
+  /**
+   * Ask the user to enter a string, or nothing to cancel.
+   *
+   * @param message
+   *          The message to display at the beginning of the input.
+   * @param emptyType
+   *          What to do when the user leaves an empty value.
+   * @param original
+   *          Optional original value, only used when using emptyType KEEP.
+   * @return a string, or null if the user canceled the input.
+   */
+  public String getStringInput(String message, EmptyType emptyType, String... original) {
+    String string = null;
+
+    System.out.print(message + " (Enter nothing to ");
+    switch (emptyType) {
+    case EMPTY:
+      System.out.print("leave empty");
+      break;
+    case CANCEL:
+      System.out.print("cancel");
+      break;
+    case KEEP:
+      System.out.print("keep current value, enter 0 to set no value");
+      break;
+    }
+    System.out.print(")\n> ");
+
+    String input = scanner.nextLine();
+    String[] words = splitToWords(input);
+
+    if ("".equals(words[0])) {
+      switch (emptyType) {
+      case EMPTY:
+        System.out.println("Value set to null.");
+        break;
+      case CANCEL:
+        System.out.println(CANCELED);
+        break;
+      case KEEP:
+        System.out.println("Original value kept.");
+        if (original != null && original.length >= 1) {
+          string = original[0];
+        }
+        break;
+      }
+    } else if (emptyType == EmptyType.KEEP && "0".equals(words[0])) {
+      System.out.println("Value set to null.");
+    } else {
+      string = input;
+      System.out.println();
+    }
+
+    return string;
+  }
+
+  /**
+   * Ask the user if he wants to do something.
+   *
+   * @param message
+   *          The message to display to the user.
+   * @return true if the user answered by yes, false otherwise.
+   */
+  public boolean isYesAnswer(String message) {
+    System.out.print(message + " [y|n]\n> ");
+
+    String input = scanner.nextLine();
+    System.out.println();
+
+    if (input == null) {
+      input = "";
+    } else {
+      input.trim().toLowerCase();
+    }
+
+    return input.startsWith("y");
+  }
+
+  /**
+   * Ask the user to enter a date, or nothing to leave empty.
+   *
+   * @param message
+   *          The message to display at the beginning of the input.
+   * @param emptyType
+   *          What to do when the user leaves an empty value.
+   * @param original
+   *          Optional original value, only used when using emptyType KEEP.
+   * @return the date, or null if the the user left the input empty.
+   */
+  public String getDateInput(String message, EmptyType emptyType, String... original) {
+    boolean running = true;
+    String date = null;
+    String string;
+
+    String datePattern = messageSource.getMessage(MSG_DATE_FORMAT, null,
+        LocaleContextHolder.getLocale());
+
+    System.out.print(message + " (" + datePattern + ", enter nothing to ");
+    switch (emptyType) {
+    case EMPTY:
+      System.out.print("leave empty");
+      break;
+    case CANCEL:
+      System.out.print("cancel");
+      break;
+    case KEEP:
+      System.out.print("keep current value, enter 0 to set no value");
+      break;
+    }
+    System.out.println(")");
+
+    // Loop until a valid date is entered or the input is canceled
+    do {
+      System.out.print("> ");
+
+      string = scanner.nextLine();
+      String[] words = splitToWords(string);
+
+      if ("".equals(words[0])) {
+        switch (emptyType) {
+        case EMPTY:
+          System.out.println("Value set to null.");
+          break;
+        case CANCEL:
+          System.out.println(CANCELED);
+          break;
+        case KEEP:
+          System.out.println("Original value kept.");
+          if (original != null && original.length >= 1) {
+            date = original[0];
+          }
+          break;
+        }
+        System.out.println();
+        running = false;
+      } else if (emptyType == EmptyType.KEEP && "0".equals(words[0])) {
+        System.out.println("Value set to null.");
+        System.out.println();
+        running = false;
+      } else {
+        boolean bad = true;
+
+        LocalDate dateIn = Util.parseLocalDate(string, datePattern);
+        if (dateIn == null) {
+          date = null;
+          bad = false;
+          System.out.println();
+        } else {
+          date = string;
+        }
+        if (bad) {
+          System.out.println("Please enter a valid date.");
+        }
+      }
+    } while (date == null && running);
+
+    return date;
+  }
+
+  /**
+   * Ask the user to input a (long) number.
+   *
+   * @param message
+   *          The message to display at the beginning of the input.
+   * @param emptyType
+   *          What to do when the user leaves an empty value.
+   * @param original
+   *          Optional original value, only used when using emptyType KEEP.
+   * @return the input number, or null if the input was canceled.
+   */
+  public Long getLongInput(String message, EmptyType emptyType, Long... original) {
+    boolean running = true;
+    Long number = null;
+    String string;
+
+    System.out.print(message + " (Positive number, enter nothing to ");
+    switch (emptyType) {
+    case EMPTY:
+      System.out.print("leave empty");
+      break;
+    case CANCEL:
+      System.out.print("cancel");
+      break;
+    case KEEP:
+      System.out.print("keep current value, enter 0 to set no value");
+      break;
+    }
+    System.out.println(")");
+
+    // Loop until a valid number is entered or the input is canceled
+    do {
+      System.out.print("> ");
+
+      string = scanner.nextLine();
+      String[] words = splitToWords(string);
+
+      if ("".equals(words[0])) {
+        switch (emptyType) {
+        case EMPTY:
+          System.out.println("Value set to null.");
+          break;
+        case CANCEL:
+          System.out.println(CANCELED);
+          break;
+        case KEEP:
+          System.out.println("Original value kept.");
+          if (original != null && original.length >= 1) {
+            number = original[0];
+          }
+          break;
+        }
+        System.out.println();
+        running = false;
+      } else if (emptyType == EmptyType.KEEP && "0".equals(words[0])) {
+        System.out.println("Value set to null.");
+        System.out.println();
+        running = false;
+      } else {
+        boolean bad = true;
+
+        // Try to parse the number
+        if (words[0].matches("[+]?[0-9]+")) {
+          number = Long.parseLong(words[0]);
+          bad = false;
+          System.out.println();
+        }
+        if (bad) {
+          System.out.println("Please enter a positive number.");
+        }
+      }
+    } while (number == null && running);
+
+    return number;
+  }
+
+  /**
+   * Split a string into words.
+   *
+   * @param string
+   *          The string to split.
+   * @return a non-empty array with all the substrings.
+   */
+  public static String[] splitToWords(String string) {
+    String[] words;
+
+    if (string != null) {
+      words = string.trim().toLowerCase().split(" ");
+    } else {
+      words = new String[1];
+      words[0] = "";
+    }
+
+    return words;
+  }
+
+  public Scanner getScanner() {
+    return scanner;
+  }
+
+  public void close() {
+    scanner.close();
+    scanner = null;
+  }
+
+  public String nextLine() {
+    if (scanner == null) {
+      return null;
+    } else {
+      return scanner.nextLine();
+    }
+  }
+}
